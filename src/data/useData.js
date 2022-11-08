b5f65af8b45ec70f7b54a1c575e5036ff742a5e2
Nicolas LEPETIT <nicolas.lepetit@pxcom.aero>
2022-11-08 13:10:11 +0100

    doc useData

--- src/data/useData.js
+++ src/data/useData.js
@@ -1,19 +1,9 @@
-
+/**
+ * Get datas from API by ID/endpoint
+ * @param {Number} id user ID
+ * @param {String} endpoint API endpoint where we need datas
+ * @returns {Promise}
+ */
 export default function getUser(id, endpoint = "/") {
     return fetch(`http://localhost:3000/user/${id}${endpoint}`).then(data => data.json().catch(error => error))
 }
-// export function getActivity() {
-//     const response = fetch('http://localhost:3000/user/12/activity')
-//     const data = response.json()
-//     return data
-// }
-// export function getAverageSessions() {
-//     const response = fetch('http://localhost:3000/user/12/average-sessions')
-//     const data = response.json()
-//     return data
-// }
-// export function getPerformance() {
-//     const response = fetch('http://localhost:3000/user/12/performance')
-//     const data = response.json()
-//     return data
-// }
