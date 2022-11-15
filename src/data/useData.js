2bae7ebc7ac3716e24a0bdd6095b931d60aa6b7e
Nicolas LEPETIT <nicolas.lepetit@pxcom.aero>
2022-11-15 14:00:22 +0100

    add jsDoc

--- src/data/useData.js
+++ src/data/useData.js
@@ -2,7 +2,7 @@
  * Get datas from API by ID/endpoint
  * @param {Number} id user ID
  * @param {String} endpoint API endpoint where we need datas
- * @returns {Promise}
+ * @return {Promise} with datas
  */
 export default function getUser(id, endpoint = "/") {
     return fetch(`http://localhost:3000/user/${id}${endpoint}`).then(data => data.json().catch(error => error))
